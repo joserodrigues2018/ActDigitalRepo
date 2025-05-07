3b78c113dd6246f3afdd5cc7598571167688f9db
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-07 17:32:12 -0400

    criação e configuração da Entidade Product na camada ORM

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public  class Product: BaseEntity
+    {
+        public string Title { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/ProductConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class ProductConfiguration: IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
+
+            builder.Property(u => u.Title).IsRequired().HasMaxLength(100);
+            builder.Property(u => u.Price).IsRequired().HasPrecision(5, 2);
+            builder.Property(u => u.Description).IsRequired().HasMaxLength(100);
+            builder.Property(u => u.Category).HasMaxLength(50);
+            builder.Property(u => u.Image).HasMaxLength(200);
+
+        }
+    }
+}
