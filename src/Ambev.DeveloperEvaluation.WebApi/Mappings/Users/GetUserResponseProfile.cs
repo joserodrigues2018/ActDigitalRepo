4f24bdcfad67d2c2ad20cfe47db0e4e1dce0ce08
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-08 14:21:38 -0400

    Arquivos foram movidos para a pasta User para organizar o projeto WebApi

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Mappings/Users/GetUserResponseProfile.cs
@@ -0,0 +1,14 @@
+﻿using Ambev.DeveloperEvaluation.Application.Users.GetUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings.Users
+{
+    public class GetUserResponseProfile : Profile
+    {
+        public GetUserResponseProfile()
+        {
+            CreateMap<GetUserResult, GetUserResponse>();
+        }
+    }
+}
