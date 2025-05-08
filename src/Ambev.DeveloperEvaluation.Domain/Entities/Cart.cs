cdd350d4f195a8f16492723172ce182c33cb0d66
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-07 21:31:01 -0400

    Criação model Entidade Carts

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Cart.cs
@@ -0,0 +1,12 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public  class Cart: BaseEntity
+    {
+        public int UserId { get; set; }
+        public DateTime DateRegistro { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
