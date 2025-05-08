763c41f9d6965d6d17516ef5b7518f76bf95d923
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-08 19:58:15 -0400

    Implementação camada Repository Crud Carts

---
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ICartRepository.cs
@@ -0,0 +1,24 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories
+{
+    public interface ICartRepository
+    {
+        /// <summary>
+        /// Creates a new Cart in the repository
+        /// </summary>
+        /// <param name="Cart">The Cart to create</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The created Cart</returns>
+        Task<Cart> CreateAsync(Cart Cart, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Retrieves a Cart by their unique identifier
+        /// </summary>
+        /// <param name="id">The unique identifier of the Cart</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The Cart if found, null otherwise</returns>
+        Task<Cart?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/CartRepository.cs
@@ -0,0 +1,29 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly DefaultContext _context;
+
+        public CartRepository(DefaultContext context)
+        {
+            _context = context;       
+        }
+
+        public async Task<Cart?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+           return await  _context.Carts.FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
+        }
+
+        public async Task<Cart> CreateAsync(Cart Cart, CancellationToken cancellationToken)
+        {
+           await _context.Carts.AddAsync(Cart, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Cart;
+        }
+    }
+}
