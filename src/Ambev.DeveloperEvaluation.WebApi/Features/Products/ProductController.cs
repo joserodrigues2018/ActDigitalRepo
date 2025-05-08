415e8f8aeb898ba007bf9728e4989fb83166261b
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-08 14:39:39 -0400

    correção para o nome da Controller para a Product

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Products/ProductController.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Products/ProductController.cs
@@ -1,6 +1,6 @@
-﻿using Ambev.DeveloperEvaluation.Application.Users.GetUser;
+﻿using Ambev.DeveloperEvaluation.Application.Products.GetProduct;
 using Ambev.DeveloperEvaluation.WebApi.Common;
-using Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Products.GetProduct;
 using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -9,7 +9,7 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Products
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PrroductController : BaseController
+    public class ProductController : BaseController
     {
         private readonly IMediator _mediator;
         private readonly IMapper _mapper;
@@ -19,7 +19,7 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Products
         /// </summary>
         /// <param name="mediator"></param>
         /// <param name="mapper"></param>
-        public PrroductController(IMediator mediator, IMapper mapper)
+        public ProductController(IMediator mediator, IMapper mapper)
         {
             _mediator = mediator;
             _mapper = mapper;
@@ -28,26 +28,26 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Products
 
         // GET api/<PrroductController>/5
         [HttpGet("{id}")]
-        [ProducesResponseType(typeof(ApiResponseWithData<GetUserResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponseWithData<GetProductResponse>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetUser([FromRoute] Guid id, CancellationToken cancellationToken)
+        public async Task<IActionResult> GetProduct([FromRoute] Guid id, CancellationToken cancellationToken)
         {
-            var request = new GetUserRequest { Id = id };
-            var validator = new GetUserRequestValidator();
+            var request = new GetProductResquest { Id = id };
+            var validator = new GetProductRequestValidator();
             var validationResult = await validator.ValidateAsync(request, cancellationToken);
 
             if (!validationResult.IsValid)
                 return BadRequest(validationResult.Errors);
 
-            var command = _mapper.Map<GetUserCommand>(request.Id);
+            var command = _mapper.Map<GetProductrCommand>(request.Id);
             var response = await _mediator.Send(command, cancellationToken);
 
-            return Ok(new ApiResponseWithData<GetUserResponse>
+            return Ok(new ApiResponseWithData<GetProductResponse>
             {
                 Success = true,
-                Message = "User retrieved successfully",
-                Data = _mapper.Map<GetUserResponse>(response)
+                Message = "Product retrieved successfully",
+                Data = _mapper.Map<GetProductResponse>(response)
             });
         }
     }
