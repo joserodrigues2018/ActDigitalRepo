c2992f149a47a433cf0c2d2c661c37e0677420d9
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-10 08:41:05 -0400

    Criação das models Sales Order para atender as vendas

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/SaleOrderItem.cs
@@ -0,0 +1,16 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class SaleOrderItem: BaseEntity
+    {
+        public Guid SaleOrderId { get; set; }
+        public string? ProductId { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public decimal ValueTotIten { get; set; }
+        public string? StatusIten { get; set; }
+
+        public SaleOrder? SaleOrderNav { get; set; }
+    }
+}
