5fafe485b11f508d5e899530f5bad49ce9120267
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-13 10:41:51 -0400

    Atualização do Configurariosn e UpdateMigrations das entidades Cart e CartItem

---
+++ src/Ambev.DeveloperEvaluation.ORM/Migrations/20250513143840_UpdateConfigurationsCartMigration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateConfigurationsCartMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItens_Products_ProductCartNavId",
+                table: "CartItens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItens_ProductCartNavId",
+                table: "CartItens");
+
+            migrationBuilder.DropColumn(
+                name: "ProductCartNavId",
+                table: "CartItens");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductCartNavId",
+                table: "CartItens",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItens_ProductCartNavId",
+                table: "CartItens",
+                column: "ProductCartNavId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItens_Products_ProductCartNavId",
+                table: "CartItens",
+                column: "ProductCartNavId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
