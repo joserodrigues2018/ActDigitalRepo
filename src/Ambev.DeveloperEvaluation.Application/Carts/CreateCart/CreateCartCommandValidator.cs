7b00302e98a68d08e111cb0cdc1ecbffcce5a8fc
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-13 19:51:02 -0400

    Implementação de validações na CreateCartRequest e na CreateCartCommand

--- src/Ambev.DeveloperEvaluation.Application/Carts/CreateCart/CreateCartCommandValidator.cs
+++ src/Ambev.DeveloperEvaluation.Application/Carts/CreateCart/CreateCartCommandValidator.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Validation;
+using FluentValidation;
 
 namespace Ambev.DeveloperEvaluation.Application.Carts.CreateCart
 {
@@ -10,6 +12,15 @@ namespace Ambev.DeveloperEvaluation.Application.Carts.CreateCart
         public CreateCartCommandValidator()
         {
             RuleFor(cart => cart.UserId).NotEmpty().WithMessage("UserId is required");
+            RuleFor(cart => cart.CartItens).NotNull().WithMessage("CartItems is requiride");
+
+            RuleForEach(cart => cart.CartItens).ChildRules(item => 
+            {
+                item.RuleFor(x => x.Quantity < 0).NotNull().WithMessage("Quanmtity is negative");
+                item.RuleFor(x => x.Quantity).NotNull().NotEqual(0).WithMessage("Qunatity is required");
+                item.RuleFor(x => x.Quantity).NotNull().GreaterThanOrEqualTo(1).LessThanOrEqualTo(20)
+                                             .WithMessage("Maximum limit: 20 items per product");
+            });
         }
     }
 }
