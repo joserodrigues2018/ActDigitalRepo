01d97e79ff6dcb2e1e89de38f733362b3701d2d1
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-21 15:34:04 -0400

    remoção de arquivos de classes para o CalcDiscount não mais usada.

--- src/Ambev.DeveloperEvaluation.Domain/Services/ICalcDiscount.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ambev.DeveloperEvaluation.Domain.Services
-{
-    public interface ICalcDiscount
-    {
-       Task<decimal> ApplyDiscount(decimal unitPrice, int quantity, int percent);
-    }
-}
