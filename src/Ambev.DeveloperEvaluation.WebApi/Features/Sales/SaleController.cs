a90755d0ead272a8844e57f6cc54faa298c44700
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-23 12:57:34 -0400

    Implementação camda controller SaleOrder para listar as Sales e SalesItem

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SaleController.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SaleController.cs
@@ -1,6 +1,9 @@
 ﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Sales.GetSale;
 using Ambev.DeveloperEvaluation.WebApi.Common;
 using Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
 using AutoMapper;
 using FluentValidation;
 using MediatR;
@@ -29,7 +32,7 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales
         [HttpPost]
         [ProducesResponseType(typeof(ApiResponseWithData<CreateSaleResponse>), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> CreateSale([FromBody] CreateSaleResquest request, CancellationToken cancellationToken)
+        public async Task<IActionResult> CreateSaleOrder([FromBody] CreateSaleResquest request, CancellationToken cancellationToken)
         {
             var validator = new CreatetSaleRequestValidator();
             var validationResult = await validator.ValidateAsync(request, cancellationToken);
@@ -47,5 +50,35 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales
                 Data = _mapper.Map<CreateSaleResponse>(response)
             });
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ApiResponseWithData<GetSaleResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetSaleOrder([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var request = new GetSaleResquest { Id = id };
+            var validator = new GetSaleRequestValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (!validationResult.IsValid)
+                return BadRequest(validationResult.Errors);
+
+            var command = _mapper.Map<GetSaleCommand>(request.Id);
+            var response = await _mediator.Send(command, cancellationToken);
+
+            return Ok(new ApiResponseWithData<GetSaleResponse>
+            {
+                Success = true,
+                Message = "Sale retrieved successfully",
+                Data = _mapper.Map<GetSaleResponse>(response)
+            });
+        }
+
     }
 }
