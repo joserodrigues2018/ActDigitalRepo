d35f98890fb5fc65694f47c2f35c266a322befd0
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-22 22:18:05 -0400

    Criação das Entidades para as vendas baseadas nos registros do Cart e CartItem

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/SaleOrder.cs
@@ -0,0 +1,24 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public  class SaleOrder: BaseEntity
+    {
+        public Guid CartId { get; set; }
+        public int NumeroVenda { get; set; }
+        public DateTime SaleDate { get; set; }
+        public string? Client { get; set; }
+        public decimal ValueTotal { get; set; }
+        public string? Filial { get; set; }
+        public SaleStatus Status { get; set; }
+
+        public IList<SaleOrderItem>? SaleOrderItems { get; set; }
+
+        public SaleOrder()
+        {
+            SaleDate = DateTime.UtcNow;
+        }
+
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Enums/SaleOrderItem.cs
@@ -0,0 +1,14 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Enums
+{
+    public class SaleOrderItem: BaseEntity
+    {
+        public Guid SaleId { get; set; }
+        public string? ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal ValueTotIten { get; set; }
+        public int PercentDiscount { get; set; }
+    }
+}
