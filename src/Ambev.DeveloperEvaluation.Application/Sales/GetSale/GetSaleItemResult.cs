73a8b8ca1eae0435cef49102842eae166e085bec
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-23 12:49:00 -0400

    Ajustes para os atributos usados para o retorno da consulta na Entidades SaleOrder e SaleOrderItem

--- src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleItemResult.cs
+++ src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleItemResult.cs
@@ -6,5 +6,8 @@
         public Guid SaleOrderId { get; set; }
         public string? ProductId { get; set; }
         public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal ValueTotIten { get; set; }
+        public int PercentDiscount { get; set; }
     }
 }
--- src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleResult.cs
+++ src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleResult.cs
@@ -1,11 +1,18 @@
-﻿namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale
+﻿using Ambev.DeveloperEvaluation.Domain.Enums;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale
 {
     public class GetSaleResult
     {
         public Guid Id { get; set; }
-        public string? UserId { get; set; }
+        public Guid CartId { get; set; }
+        public int NumeroVenda { get; set; }
         public DateTime SaleDate { get; set; }
+        public string? Client { get; set; }
+        public decimal ValueTotal { get; set; }
+        public string? Filial { get; set; }
+        public SaleStatus Status { get; set; }
 
-        public IList<GetSaleItemResult>? SaleOrderItems { get; set; } = [];
+        public IList<GetSaleItemResult>? SaleOrderItems { get; set; }
     }
 }
