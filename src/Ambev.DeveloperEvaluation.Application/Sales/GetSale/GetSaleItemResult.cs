2e41c874b42607eac3a9ec4870b0c53dee98c049
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-23 12:38:40 -0400

    Implementação da camada Application para o Listar vendas pelo Id.

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleItemResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale
+{
+    public class GetSaleItemResult
+    {
+        public Guid Id { get; set; }
+        public Guid SaleOrderId { get; set; }
+        public string? ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale
+{
+    public class GetSaleResult
+    {
+        public Guid Id { get; set; }
+        public string? UserId { get; set; }
+        public DateTime SaleDate { get; set; }
+
+        public IList<GetSaleItemResult>? SaleOrderItems { get; set; } = [];
+    }
+}
