e1cec22dab0c11638fd7df40133dfbf852e24d66
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-22 23:09:33 -0400

    Implementação do repository para persistência dos dados nas models Sales

---
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories
+{
+    public interface ISaleOrderRepository
+    {
+        /// <summary>
+        /// Add SaleOrder with percent discount for sale
+        /// </summary>
+        /// <param name="sale"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<SaleOrder> AddSaleOrder(SaleOrder sale, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/SaleOrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    public class SaleOrderRepository : ISaleOrderRepository
+    {
+        private DefaultContext _context;
+
+        public SaleOrderRepository(DefaultContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<SaleOrder> AddSaleOrder(SaleOrder sale, CancellationToken cancellationToken = default)
+        {
+            await _context.SaleOrders.Include( s => s.SaleOrderItems).ToListAsync(cancellationToken);
+
+            await _context.SaleOrders.AddRangeAsync(sale);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return sale;
+
+        }
+    }
+}
