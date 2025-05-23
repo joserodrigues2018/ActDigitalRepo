fdada7dac6ff14b95be329206473dbebc3a834e1
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-23 11:24:10 -0400

    Correção para o campo SaleId para SaleOrderId

--- src/Ambev.DeveloperEvaluation.Domain/Entities/SaleOrderItem.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/SaleOrderItem.cs
@@ -4,7 +4,7 @@ namespace Ambev.DeveloperEvaluation.Domain.Entities
 {
     public class SaleOrderItem: BaseEntity
     {
-        public Guid SaleId { get; set; }
+        public Guid SaleOrderId { get; set; }
         public string? ProductId { get; set; }
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
