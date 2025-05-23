cbdab88333f1ff723e20cb803fc9f4ac58455d64
José Rodrigues <rodrigues.santos@gmail.com>
2025-05-23 12:52:46 -0400

    Implementação das models e estrutras para o request e o response para atender as entidades SaleOrder e SaleOrderItem

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/GetSale/GetSaleItemResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale
+{
+    public class GetSaleItemResponse
+    {
+        public Guid Id { get; set; }
+        public Guid SaleOrderId { get; set; }
+        public string? ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal ValueTotIten { get; set; }
+        public int PercentDiscount { get; set; }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/GetSale/GetSaleResponse.cs
@@ -0,0 +1,19 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale
+{
+    public class GetSaleResponse
+    {
+        public Guid Id { get; set; }
+        public Guid CartId { get; set; }
+        public int NumeroVenda { get; set; }
+        public DateTime SaleDate { get; set; }
+        public string? Client { get; set; }
+        public decimal ValueTotal { get; set; }
+        public string? Filial { get; set; }
+        public SaleStatus Status { get; set; }
+
+        public IList<GetSaleItemResult>? SaleOrderItems { get; set; }
+    }
+}
