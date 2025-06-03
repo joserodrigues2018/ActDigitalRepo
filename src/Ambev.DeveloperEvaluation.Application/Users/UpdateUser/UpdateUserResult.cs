77dc03e67458a3fef4673b5210d767cc5cf0c0df
José Rodrigues <rodrigues.santos@gmail.com>
2025-06-03 17:22:36 -0400

    Implementação da camada Application para o UpdateUser

---
+++ src/Ambev.DeveloperEvaluation.Application/Users/UpdateUser/UpdateUserResult.cs
@@ -0,0 +1,43 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Enums;
+
+namespace Ambev.DeveloperEvaluation.Application.Users.UpdateUser;
+
+/// <summary>
+/// Represents the response returned after successfully creating a new user.
+/// </summary>
+/// <remarks>
+/// This response contains the unique identifier of the newly Updated user,
+/// which can be used for subsequent operations or reference.
+/// </remarks>
+public class UpdateUserResult
+{
+    /// <summary>
+    /// The unique identifier of the user
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// The user's full name
+    /// </summary>
+    public string Username { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The user's email address
+    /// </summary>
+    public string Email { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The user's phone number
+    /// </summary>
+    public string Phone { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The user's role in the system
+    /// </summary>
+    public UserRole Role { get; set; }
+
+    /// <summary>
+    /// The current status of the user
+    /// </summary>
+    public UserStatus Status { get; set; }
+}
