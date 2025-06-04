73500e7d7dad6ec9db699213c419736bb4115644
José Rodrigues <rodrigues.santos@gmail.com>
2025-06-04 14:21:36 -0400

    Alteração para validação do UpdateProduct usar somente o Id para validar

--- src/Ambev.DeveloperEvaluation.Application/Products/UpdateProduct/UpdateProductValidator.cs
+++ src/Ambev.DeveloperEvaluation.Application/Products/UpdateProduct/UpdateProductValidator.cs
@@ -6,10 +6,8 @@ public class UpdateProductCommandValidator : AbstractValidator<UpdateProductComm
 {
 	public UpdateProductCommandValidator()
 	{
-        RuleFor(product => product.Title).NotEmpty().WithMessage("title is required");
-        RuleFor(product => product.Description).NotEmpty().WithMessage("Description is required");
-        RuleFor(product => product.Price).NotEmpty().WithMessage("Price is required");
-        RuleFor(product => product.Category).NotEmpty().WithMessage("Price is required");
-
+        RuleFor(x => x.Id)
+        .NotEmpty()
+        .WithMessage("Product ID is required");
     }
 }
