9e4658f0fc5af6c991a4d1b45d4c79fc415632d9
José Rodrigues <rodrigues.santos@gmail.com>
2025-06-05 19:29:28 -0400

    Inclusão de validações no UpdateProduct para a Request e o Command

--- src/Ambev.DeveloperEvaluation.Application/Products/UpdateProduct/UpdateProductValidator.cs
+++ src/Ambev.DeveloperEvaluation.Application/Products/UpdateProduct/UpdateProductValidator.cs
@@ -6,8 +6,11 @@ public class UpdateProductCommandValidator : AbstractValidator<UpdateProductComm
 {
 	public UpdateProductCommandValidator()
 	{
-        RuleFor(x => x.Id)
-        .NotEmpty()
-        .WithMessage("Product ID is required");
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+        RuleFor(product => product.Title).NotEmpty().WithMessage("title is required");
+        RuleFor(product => product.Description).NotEmpty().WithMessage("Description is required");
+        RuleFor(product => product.Price).NotEmpty().WithMessage("Price is required");
+        RuleFor(product => product.Category).NotEmpty().WithMessage("Price is required");
+
     }
 }
--- src/Ambev.DeveloperEvaluation.WebApi/Features/Products/UpdateProduct/UpdateProductRequestValidator.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Products/UpdateProduct/UpdateProductRequestValidator.cs
@@ -6,9 +6,12 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Products.UpdateProduct
     {
         public UpdatetProductRequestValidator()
         {
-            RuleFor(x => x.Id)
-            .NotEmpty()
-            .WithMessage("Product ID is required");
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+            RuleFor(product => product.Request!.Title).NotEmpty().WithMessage("title is required");
+            RuleFor(product => product.Request!.Description).NotEmpty().WithMessage("Description is required");
+            RuleFor(product => product.Request!.Price).NotEmpty().WithMessage("Price is required");
+            RuleFor(product => product.Request!.Category).NotEmpty().WithMessage("Price is required");
+
         }
     }
 }
